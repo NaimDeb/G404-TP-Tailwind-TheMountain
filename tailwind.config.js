e8b567efcd8a73555654954d3e3ba1a2e6144c13
naimdeb <naimdebard02@gmail.com>
2024-11-04 14:00:35 +0100

    add section contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
           'off-white': 'hsl(36, 100%, 99%)',
           'gray': 'hsl(0, 0%, 33%)',
           'bg-blue': 'hsl(197, 79%, 75%, 75%)',
-          'btn-red': 'hsl(345, 91%, 55%)'
+          'btn-red': 'hsl(345, 91%, 55%)',
+          'bg-sombre': 'hsl(0, 0%, 8%, 0.4)'
       },
       },
       fontFamily: {
@@ -19,6 +20,7 @@ module.exports = {
         'hero-img': "url('../images/bg_header.jpg')",
         'about-img': "url('../images/bg_nuage.jpg')",
         'history-img': "url('../images/bg_appareil_photo.jpg')",
+        'contact-img': "url('../images/bg_newsletter.jpg')",
       },
     },
   },
